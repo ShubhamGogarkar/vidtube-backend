a289dd357c2ce9f932b98b87f119f6787de88417
ShubhamGogarkar <shubhamgogarkar22@gmail.com>
2026-08-19 17:19:46 +0530

    fix: update CORS configuration to enhance security with sameSite and secure options

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ import cookieParser from "cookie-parser"
 
 
 const app = express()
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
+app.set('trust proxy', 1);
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true, sameSite:'none', secure: true }))
 app.use(express.json({limit: "20kb"}));
 app.use(express.urlencoded({ extended: true,limit: "20kb"  }));
 app.use(express.static("public"))
